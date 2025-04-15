15092af019067ca189f510de9a59ee6ccdc50db4
Caio Henrique Ramos de Souza <caiohenriquebr12@gmail.com>
2025-04-15 03:31:33 -0300

    atualizando e estilizando o portifólio

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 // Aguarda o carregamento completo da página
 document.addEventListener("DOMContentLoaded", function () {
-    // ========== MODO NOTURNO ==========
-    const toggleButton = document.getElementById("theme-toggle");
-    const body = document.body;
-
-    // Define o tema padrão ao carregar
-    body.classList.add("light-theme");
-
-    toggleButton.addEventListener("click", function () {
-        if (body.classList.contains("light-theme")) {
-            body.classList.remove("light-theme");
-            body.classList.add("dark-theme");
-            toggleButton.textContent = "🌞 Modo Matutino";
-        } else {
-            body.classList.remove("dark-theme");
-            body.classList.add("light-theme");
-            toggleButton.textContent = "🌙Modo Noturno";
-        }
-    });
 
     // ========== SCROLL SUAVE ==========
     document.querySelectorAll("nav ul li a").forEach((anchor) => {
